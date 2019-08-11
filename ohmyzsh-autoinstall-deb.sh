480d283f1e04ea9c0dc4ae3eb3df8c511e504310
Jedi Fonseca <jedifn7@gmail.com>
2019-08-11 18:37:59 -0300

    Update ohmyzsh-autoinstall-deb.sh

--- ohmyzsh-autoinstall-deb.sh
+++ ohmyzsh-autoinstall-deb.sh
@@ -1,6 +1,7 @@
 !/bin/bash
 
 sudo apt install zsh wget git fonts-powerline -y
+zenity --info --title=Warning --text="We'll now install 'Oh My Zshell'.\n\nIMPORTANT: Once the installation is finished please DO NOT close the terminal. Type 'exit', press 'Enter', and then you may close it." --width=600 --height=120
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo sed -i 's/bash/zsh/g' /etc/passwd
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' /home/"$USER"/.zshrc
