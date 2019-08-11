d555f0d2448afeacd12db637d7e9868932282858
Jedi Fonseca <jedifn7@gmail.com>
2019-08-10 21:26:46 -0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+!/bin/bash
 
+sudo su
 apt install zsh wget git fonts-powerline -y
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' /home/"$USER"/.zshrc
 sed -i 's/bash/zsh/g' /etc/passwd
+exit
+exit
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' /home/"$USER"/.zshrc
